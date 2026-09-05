5c536ac8b3d600cc52f41f326f318e7100d58e6a
cyr <chendawn03@qq.com>
2026-09-05 14:50:48 +0800

    chore: export HA evidence adapter

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,18 @@
 """Meta-Improver application package."""
+from .ha_evidence import (
+    HAEvidenceArtifact,
+    HAEvidenceError,
+    HATargetEvidence,
+    evidence_to_attribution,
+    read_ha_target_evidence,
+    rehydrate_ha_target_evidence,
+)
 
+__all__ = [
+    "HAEvidenceArtifact",
+    "HAEvidenceError",
+    "HATargetEvidence",
+    "evidence_to_attribution",
+    "read_ha_target_evidence",
+    "rehydrate_ha_target_evidence",
+]
